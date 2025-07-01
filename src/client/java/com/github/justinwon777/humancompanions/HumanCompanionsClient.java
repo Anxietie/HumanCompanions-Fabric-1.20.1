2e404d6f06b82a6a70efeb38c40ffba715310d2b
anxietie <84636109+Anxietie@users.noreply.github.com>
2025-06-30 20:17:54 -0400

    forge -> fabric (build tools)

---
+++ src/client/java/com/github/justinwon777/humancompanions/HumanCompanionsClient.java
@@ -0,0 +1,10 @@
+package com.github.justinwon777.humancompanions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HumanCompanionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
