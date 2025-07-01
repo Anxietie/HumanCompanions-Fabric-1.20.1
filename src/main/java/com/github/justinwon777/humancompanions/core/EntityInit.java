c7d8fa0109462755b001e94a4f498aa5c408cd2e
anxietie <84636109+Anxietie@users.noreply.github.com>
2025-06-30 20:17:56 -0400

    ported entity registration to fabric

--- src/main/java/com/github/justinwon777/humancompanions/core/EntityInit.java
+++ src/main/java/com/github/justinwon777/humancompanions/core/EntityInit.java
@@ -5,37 +5,44 @@ import com.github.justinwon777.humancompanions.entity.Arbalist;
 import com.github.justinwon777.humancompanions.entity.Archer;
 import com.github.justinwon777.humancompanions.entity.Axeguard;
 import com.github.justinwon777.humancompanions.entity.Knight;
-import net.minecraft.core.registries.Registries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
 
 public final class EntityInit {
+    public static final EntityType<Arbalist> ARBALIST = register("arbalist", EntityType.Builder.of(
+            Arbalist::new,
+            MobCategory.AMBIENT)
+            .sized(0.6f, 1.8f)
+    );
+    public static final EntityType<Archer> ARCHER = register("archer", EntityType.Builder.of(
+            Archer::new,
+            MobCategory.AMBIENT)
+            .sized(0.6f, 1.8f)
+    );
+    public static final EntityType<Axeguard> AXEGUARD = register("axeguard", EntityType.Builder.of(
+            Axeguard::new,
+            MobCategory.AMBIENT)
+            .sized(0.6f, 1.8f)
+    );
+    public static final EntityType<Knight> KNIGHT = register("knight", EntityType.Builder.of(
+                    Knight::new,
+                    MobCategory.AMBIENT)
+            .sized(0.6f, 1.8f)
+    );
 
-    private EntityInit() {}
+    public static void registerEntities() {}
 
-    public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(Registries.ENTITY_TYPE,
-            HumanCompanions.MOD_ID);
-
-    public static final RegistryObject<EntityType<Knight>> Knight =
-            ENTITIES.register("knight", () -> EntityType.Builder.of(Knight::new, MobCategory.AMBIENT)
-                    .sized(0.6F, 1.8F)
-                    .build(new ResourceLocation(HumanCompanions.MOD_ID, "knight").toString()));
-
-    public static final RegistryObject<EntityType<Archer>> Archer =
-            ENTITIES.register("archer", () -> EntityType.Builder.of(Archer::new, MobCategory.AMBIENT)
-                    .sized(0.6F, 1.8F)
-                    .build(new ResourceLocation(HumanCompanions.MOD_ID, "archer").toString()));
-
-    public static final RegistryObject<EntityType<Arbalist>> Arbalist =
-            ENTITIES.register("arbalist", () -> EntityType.Builder.of(Arbalist::new, MobCategory.AMBIENT)
-                    .sized(0.6F, 1.8F)
-                    .build(new ResourceLocation(HumanCompanions.MOD_ID, "arbalist").toString()));
-
-    public static final RegistryObject<EntityType<Axeguard>> Axeguard =
-            ENTITIES.register("axeguard", () -> EntityType.Builder.of(Axeguard::new, MobCategory.AMBIENT)
-                    .sized(0.6F, 1.8F)
-                    .build(new ResourceLocation(HumanCompanions.MOD_ID, "axeguard").toString()));
+    private static <T extends Entity> EntityType<T> register(String path, EntityType.Builder<T> builder) {
+        return Registry.register(BuiltInRegistries.ENTITY_TYPE, new ResourceLocation(HumanCompanions.MOD_ID, path), builder.build(path));
+    }
 }
